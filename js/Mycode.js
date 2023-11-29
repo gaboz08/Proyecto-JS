af6cac848032c63d32a946b7567005edaf93ab9a
Gabriel Zorrilla <zorrillagabriel08@gmail.com>
2023-11-28 21:08:37 -0300

    segundo

--- js/Mycode.js
+++ js/Mycode.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const addProductBtn = document.getElementById('addProductBtn');
     const calculateTotalBtn = document.getElementById('calculateTotalBtn');
+    const applyDiscountCheckbox = document.getElementById('applyDiscount');
+    const applyTaxCheckbox = document.getElementById('applyTax');
 
     addProductBtn.addEventListener('click', addProductInput);
     calculateTotalBtn.addEventListener('click', calculateTotal);
@@ -9,23 +11,58 @@ document.addEventListener('DOMContentLoaded', function() {
 function addProductInput() {
     const productInputs = document.getElementById('productInputs');
     const newProductDiv = document.createElement('div');
-    newProductDiv.classList.add('product');
+    newProductDiv.classList.add('product', 'input-group', 'mb-3');
 
     newProductDiv.innerHTML = `
-        <input type="text" placeholder="Nombre del Producto/Servicio" name="productName[]">
-        <input type="number" placeholder="Precio" name="productPrice[]">
+        <input type="text" class="form-control" placeholder="Nombre del Producto/Servicio" name="productName[]">
+        <input type="number" class="form-control" placeholder="Precio" name="productPrice[]">
     `;
 
     productInputs.appendChild(newProductDiv);
 }
 
+function aplicarDescuento(precio, aplicarDescuento) {
+    if (aplicarDescuento) {
+        const descuento = Math.random() * 0.2; // Descuento de hasta un 20%
+        return precio - (precio * descuento);
+    }
+    return precio;
+}
+
+function calcularImpuesto(total, aplicarImpuesto) {
+    if (aplicarImpuesto) {
+        const impuesto = 0.15; // 15% de impuesto
+        return total * impuesto;
+    }
+    return 0;
+}
+
 function calculateTotal() {
-    let total = 0;
+    let totalSinImpuestos = 0;
+    const applyDiscount = document.getElementById('applyDiscount').checked;
+    const applyTax = document.getElementById('applyTax').checked;
     const prices = document.querySelectorAll('input[name="productPrice[]"]');
 
     prices.forEach(priceInput => {
-        total += Number(priceInput.value);
+        let precioConDescuento = aplicarDescuento(Number(priceInput.value), applyDiscount);
+        totalSinImpuestos += precioConDescuento;
     });
 
-    document.getElementById('totalCostDisplay').innerText = `Total: $${total.toFixed(2)}`;
+    let totalConImpuestos = totalSinImpuestos + calcularImpuesto(totalSinImpuestos, applyTax);
+
+    let detalleCalculo = '';
+    prices.forEach((priceInput, index) => {
+        let precioOriginal = Number(priceInput.value);
+        let precioConDescuento = aplicarDescuento(precioOriginal, applyDiscount);
+        detalleCalculo += `Producto ${index + 1}: Precio Original: $${precioOriginal.toFixed(2)}, Precio con Descuento: $${precioConDescuento.toFixed(2)}<br>`;
+    });
+
+    detalleCalculo += `Total sin Impuestos: $${totalSinImpuestos.toFixed(2)}<br>`;
+    if (applyTax) {
+        detalleCalculo += `Impuesto: $${calcularImpuesto(totalSinImpuestos, applyTax).toFixed(2)}<br>`;
+    }
+    detalleCalculo += `Total con Impuestos: $${totalConImpuestos.toFixed(2)}`;
+
+    document.getElementById('totalCostDisplay').innerText = `Total: $${totalConImpuestos.toFixed(2)}`;
+    document.getElementById('detalleCalculo').innerHTML = detalleCalculo;
 }
