42360a199e1b30f4c590698bb3f1bf1359d52244
Gabriel Zorrilla <zorrillagabriel08@gmail.com>
2023-12-14 22:12:35 -0300

    segunda preentrega

--- js/Mycode.js
+++ js/Mycode.js
@@ -1,68 +1,136 @@
+// Mycode.js
+// Este código maneja la lógica de la interfaz de usuario para añadir productos y calcular el total
+
+// Añadir un listener para cuando se cargue el documento
 document.addEventListener('DOMContentLoaded', function() {
+    // Obtener referencias a los botones y añadir event listeners
     const addProductBtn = document.getElementById('addProductBtn');
     const calculateTotalBtn = document.getElementById('calculateTotalBtn');
-    const applyDiscountCheckbox = document.getElementById('applyDiscount');
-    const applyTaxCheckbox = document.getElementById('applyTax');
 
     addProductBtn.addEventListener('click', addProductInput);
     calculateTotalBtn.addEventListener('click', calculateTotal);
 });
 
+// Función para añadir un nuevo input de producto al formulario
 function addProductInput() {
     const productInputs = document.getElementById('productInputs');
     const newProductDiv = document.createElement('div');
     newProductDiv.classList.add('product', 'input-group', 'mb-3');
 
+    // Agregar inputs para nombre, precio y cantidad del producto
     newProductDiv.innerHTML = `
-        <input type="text" class="form-control" placeholder="Nombre del Producto/Servicio" name="productName[]">
-        <input type="number" class="form-control" placeholder="Precio" name="productPrice[]">
+        <div class="autocomplete">
+            <input type="text" class="form-control productName" placeholder="Nombre del Producto/Servicio" name="productName[]">
+            <div class="autocomplete-results"></div>
+        </div>
+        <input type="number" class="form-control productPrice" placeholder="Precio" name="productPrice[]">
+        <input type="number" class="form-control" placeholder="Cantidad" name="productQuantity[]" min="1" value="1">
     `;
 
     productInputs.appendChild(newProductDiv);
+
+    // Añadir evento de autocompletado al input de nombre de producto
+    let productNameInput = newProductDiv.querySelector('.productName');
+    productNameInput.addEventListener('input', function() {
+        autocompleteProduct(this);
+    });
 }
 
+// Función para mostrar sugerencias de autocompletado
+function autocompleteProduct(inputElement) {
+    let autocompleteResults = inputElement.nextElementSibling;
+    autocompleteResults.innerHTML = '';
+
+    // Filtrar productos basados en la entrada del usuario
+    let filteredProducts = productos.filter(p => p.nombre.toLowerCase().includes(inputElement.value.toLowerCase()));
+
+    // Mostrar los resultados filtrados como opciones de autocompletado
+    filteredProducts.forEach(product => {
+        let div = document.createElement('div');
+        div.classList.add('autocomplete-item');
+        div.textContent = product.nombre;
+        div.onclick = function() {
+            selectProduct(inputElement, product, autocompleteResults);
+        };
+        autocompleteResults.appendChild(div);
+    });
+
+    // Hacer el campo de precio editable si no hay coincidencias
+    if (filteredProducts.length === 0) {
+        inputElement.parentElement.nextElementSibling.removeAttribute('readonly');
+    } else {
+        inputElement.parentElement.nextElementSibling.setAttribute('readonly', true);
+    }
+}
+
+// Función para manejar la selección de un producto desde las sugerencias
+function selectProduct(inputElement, product, autocompleteResults) {
+    // Establecer el valor del input del nombre y el precio
+    inputElement.value = product.nombre;
+    let priceInput = inputElement.parentElement.nextElementSibling;
+    priceInput.value = product.precio;
+    autocompleteResults.innerHTML = '';
+}
+
+// Función para aplicar un descuento aleatorio
 function aplicarDescuento(precio, aplicarDescuento) {
     if (aplicarDescuento) {
-        const descuento = Math.random() * 0.2; // Descuento de hasta un 20%
+        const descuento = Math.random() * 0.2; // Descuento aleatorio hasta un 20%
         return precio - (precio * descuento);
     }
     return precio;
 }
 
+// Función para calcular el impuesto
 function calcularImpuesto(total, aplicarImpuesto) {
     if (aplicarImpuesto) {
-        const impuesto = 0.15; // 15% de impuesto
-        return total * impuesto;
+        const impuesto = 0.15; // Impuesto del 15%
+        return total + (total * impuesto);
     }
-    return 0;
+    return total;
 }
 
+// Función para calcular el total de los productos añadidos
 function calculateTotal() {
     let totalSinImpuestos = 0;
     const applyDiscount = document.getElementById('applyDiscount').checked;
     const applyTax = document.getElementById('applyTax').checked;
+    const productNames = document.querySelectorAll('input[name="productName[]"]');
     const prices = document.querySelectorAll('input[name="productPrice[]"]');
+    const quantities = document.querySelectorAll('input[name="productQuantity[]"]');
 
-    prices.forEach(priceInput => {
-        let precioConDescuento = aplicarDescuento(Number(priceInput.value), applyDiscount);
-        totalSinImpuestos += precioConDescuento;
+    productNames.forEach((productNameInput, index) => {
+        if (productNameInput.value) {
+            let precioOriginal = Number(prices[index].value) || 0;
+            let cantidad = Number(quantities[index].value) || 1;
+            let precioConDescuento = aplicarDescuento(precioOriginal, applyDiscount);
+            let subtotal = precioConDescuento * cantidad;
+            totalSinImpuestos += subtotal;
+        }
     });
 
-    let totalConImpuestos = totalSinImpuestos + calcularImpuesto(totalSinImpuestos, applyTax);
+    let totalConImpuestos = calcularImpuesto(totalSinImpuestos, applyTax);
+    mostrarDetalleCalculo(productNames, prices, quantities, totalSinImpuestos, totalConImpuestos, applyTax);
+}
 
+// Función para mostrar los detalles del cálculo en la interfaz de usuario
+function mostrarDetalleCalculo(productNames, prices, quantities, totalSinImpuestos, totalConImpuestos, applyTax) {
     let detalleCalculo = '';
-    prices.forEach((priceInput, index) => {
-        let precioOriginal = Number(priceInput.value);
-        let precioConDescuento = aplicarDescuento(precioOriginal, applyDiscount);
-        detalleCalculo += `Producto ${index + 1}: Precio Original: $${precioOriginal.toFixed(2)}, Precio con Descuento: $${precioConDescuento.toFixed(2)}<br>`;
+    productNames.forEach((productNameInput, index) => {
+        if (productNameInput.value) {
+            let precioOriginal = Number(prices[index].value) || 0;
+            let cantidad = Number(quantities[index].value) || 1;
+            let precioConDescuento = aplicarDescuento(precioOriginal, document.getElementById('applyDiscount').checked);
+            detalleCalculo += `Producto ${index + 1}: ${productNameInput.value} - Precio Original: $${precioOriginal.toFixed(2)}, Cantidad: ${cantidad}, Precio con Descuento: $${precioConDescuento.toFixed(2)}, Subtotal: $${(precioConDescuento * cantidad).toFixed(2)}<br>`;
+        }
     });
 
     detalleCalculo += `Total sin Impuestos: $${totalSinImpuestos.toFixed(2)}<br>`;
     if (applyTax) {
-        detalleCalculo += `Impuesto: $${calcularImpuesto(totalSinImpuestos, applyTax).toFixed(2)}<br>`;
+        detalleCalculo += `Impuesto: $${(totalConImpuestos - totalSinImpuestos).toFixed(2)}<br>`;
     }
     detalleCalculo += `Total con Impuestos: $${totalConImpuestos.toFixed(2)}`;
 
-    document.getElementById('totalCostDisplay').innerText = `Total: $${totalConImpuestos.toFixed(2)}`;
+    document.getElementById('totalCostDisplay').innerHTML = `Total: $${totalConImpuestos.toFixed(2)}`;
     document.getElementById('detalleCalculo').innerHTML = detalleCalculo;
 }
---
+++ js/productos.js
@@ -0,0 +1,24 @@
+
+// Este array contiene productos para armar una PC
+let productos = [
+    { nombre: "Procesador Intel Core i7", precio: 300, cantidad: 10 },
+    { nombre: "Tarjeta Madre ASUS", precio: 150, cantidad: 15 },
+    { nombre: "Memoria RAM 16GB", precio: 80, cantidad: 20 },
+    { nombre: "Disco Duro SSD 1TB", precio: 120, cantidad: 12 },
+    { nombre: "Tarjeta Gráfica NVIDIA RTX 3070", precio: 500, cantidad: 5 },
+    { nombre: "Fuente de Poder 750W", precio: 100, cantidad: 10 },
+    { nombre: "Gabinete ATX", precio: 90, cantidad: 8 },
+    { nombre: "Monitor 27 pulgadas", precio: 250, cantidad: 7 },
+    { nombre: "Teclado Mecánico", precio: 60, cantidad: 15 },
+    { nombre: "Mouse Gamer", precio: 40, cantidad: 20 },
+    { nombre: "Alfombrilla Mouse", precio: 15, cantidad: 25 },
+    { nombre: "Auriculares Gamer", precio: 70, cantidad: 10 },
+    { nombre: "Cámara Web HD", precio: 50, cantidad: 10 },
+    { nombre: "Micrófono USB", precio: 35, cantidad: 10 },
+    { nombre: "Sistema de Enfriamiento", precio: 70, cantidad: 12 },
+    { nombre: "Kit de Cableado", precio: 20, cantidad: 30 },
+    { nombre: "Ventiladores Adicionales", precio: 25, cantidad: 20 },
+    { nombre: "Pasta Térmica", precio: 10, cantidad: 50 },
+    { nombre: "Adaptador WiFi USB", precio: 30, cantidad: 15 },
+    { nombre: "Tarjeta de Sonido", precio: 45, cantidad: 10 }
+];
